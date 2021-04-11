0421a9d4afff14468d0fcd5d82c5cc1ee5291329
Kharitonov Egor <yegor.kharitonov@yandex.ru>
2021-04-11 20:59:16 +0300

    проверка на прокси

--- Bitclout/Worker/ChromeWorker.cs
+++ Bitclout/Worker/ChromeWorker.cs
@@ -216,9 +216,17 @@ namespace Bitclout
                 PhoneWorker.NumberConformation(pn);//Подтверждаем номер
                 Thread.Sleep(MainWindowViewModel.settings.DelayTime);
 
-                NLog.LogManager.GetCurrentClassLogger().Info($"Кликаем дальше");
-                RegChromeDriver.FindElement(By.XPath("//a[@class='btn btn-primary font-weight-bold fs-15px ml-10px']")).Click();//Кликаем дальше
-                Thread.Sleep(MainWindowViewModel.settings.DelayTime * 2);
+                try
+                {
+                    NLog.LogManager.GetCurrentClassLogger().Info($"Кликаем дальше");
+                    RegChromeDriver.FindElement(By.XPath("//a[@class='btn btn-primary font-weight-bold fs-15px ml-10px']")).Click();//Кликаем дальше
+                    Thread.Sleep(MainWindowViewModel.settings.DelayTime * 2);
+                }
+                catch (Exception ex)
+                {
+                    MainWindowViewModel.settings.CurrentProxy.AccountsRegistred = 2;
+                    throw ex;
+                }
 
                 NLog.LogManager.GetCurrentClassLogger().Info($"Кликаем скип");
                 RegChromeDriver.FindElement(By.XPath("//button[@class='btn btn-outline-primary font-weight-bold fs-15px']")).Click();//Кликаем скип
