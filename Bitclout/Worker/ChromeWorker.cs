6067fd707f0ab467870261a95b14ba3f3ccdfbc7
Kharitonov Egor <yegor.kharitonov@yandex.ru>
2021-04-23 07:07:44 +0300

    ошибка авторизации

--- Bitclout/Worker/ChromeWorker.cs
+++ Bitclout/Worker/ChromeWorker.cs
@@ -85,6 +85,9 @@ namespace Bitclout
             BitcloutChromeDriver.FindElement(By.XPath("//textarea[@class='form-control fs-15px ng-untouched ng-pristine ng-valid']")).SendKeys(phrase);
             Thread.Sleep(MainWindowViewModel.settings.DelayTime);
 
+            BitcloutChromeDriver.FindElement(By.XPath("//button[@class='btn btn-primary font-weight-bold fs-15px']")).Click();
+            Thread.Sleep(MainWindowViewModel.settings.DelayTime);
+
             NLog.LogManager.GetCurrentClassLogger().Info($"Выбираем аккаунт");
             BitcloutChromeDriver.FindElement(By.XPath("//div[@class='d-flex justify-content-between w-100']")).Click();
             Thread.Sleep(MainWindowViewModel.settings.DelayTime);
